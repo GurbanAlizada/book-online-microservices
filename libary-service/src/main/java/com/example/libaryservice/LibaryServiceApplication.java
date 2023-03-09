f401fb7356b19f31ca465584f7d9bf4a09277696
GurbanAlizada <88265011+GurbanAlizada@users.noreply.github.com>
2023-03-09 21:34:36 +0400

    Initial commit

---
+++ libary-service/src/main/java/com/example/libaryservice/LibaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.libaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibaryServiceApplication.class, args);
+	}
+
+}
---
+++ libary-service/src/test/java/com/example/libaryservice/LibaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
